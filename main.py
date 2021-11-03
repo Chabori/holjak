c64f5b67cdee052db743eb017a2b9dc9a29821ca
Chabori <93636736+Chabori@users.noreply.github.com>
2021-11-03 15:29:34 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import random
+
 print("오징어 게임에 오신 것을 환영합니다")
 print("홀짝 게임을 시작합니다")
 
 # 구슬로 홀짝 게임 규칙
-# 상대방이 구슬의 갯수를 정한다
-a = 5
-print(a)
 # 내가 홀 또는 짝을 얘기한다
-my = "홀"
+my = input("홀 or 짝? ")
 print(my)
+# 상대방이 구슬의 갯수를 정한다
+a = random.randint(1,10)
+print(a)
 # 상대방의 구슬의 갯수에서 2로 나누어서 나머지가 0이면 짝
 # 나머지가 1이면 홀
 if a % 2 == 0:
@@ -21,4 +23,4 @@ else:
 if dab == my:
     print("맞다")
 else:
-    print("틀리다")
+    print("빵!")
